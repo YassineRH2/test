6af7c5828348ad7881872f2074b189bb059e9106
YassineRH2 <68960485+YassineRH2@users.noreply.github.com>
2020-07-30 11:41:58 +0100

    Add files via upload

---
+++ demo/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Test by YassineRH',
+    'version': '13.0',
+    'category': 'Tools',
+    'summary': 'Test for Future Projects',
+    'description': """
+        This module is just a test
+    """,
+    'author': 'YassineRH',
+    'website': 'https://www.google.com',
+    'depends': ['base'],
+    'data': [
+        'views/view.xml',
+    ],
+    'images': [
+        'pictures/image.jpg',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3'
+}
